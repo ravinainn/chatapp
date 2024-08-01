9a957893f461e53a04fb71ea1416e37e1787f7d7
ravinainn <nain.ravi12345@gmail.com>
2024-08-01 17:16:25 +0530

    change name of the folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CreateRoom from "./components/CreateRoom";
-import ChatRoom from "./components/test";
+import ChatRoom from "./components/ChatRoom";
 
 function App() {
   return (
